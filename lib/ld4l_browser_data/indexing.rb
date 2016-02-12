4b65f9583bbe0de23036a67ca24386778517896c
Jim Blake <jeb228@cornell.edu>
2016-02-12 12:35:52 -0500

    Shuffle some stuff around.

--- lib/ld4l_browser_data/indexing.rb
+++ lib/ld4l_browser_data/indexing.rb
@@ -10,15 +10,16 @@ require "ld4l_browser_data/utilities/solr_server"
 require "ld4l_browser_data/utilities/solr_server_user"
 require "ld4l_browser_data/utilities/triple_store_user"
 
+require_relative "indexing/common/counts"
+require_relative "indexing/common/language_reference"
+require_relative "indexing/common/query_runner"
+require_relative "indexing/common/topic_reference"
+
 require_relative "indexing/index_documents"
 require_relative "indexing/build_solr_index"
 require_relative "indexing/sample_solr_index"
-require_relative "indexing/counts"
 require_relative "indexing/index_specific_uris"
-require_relative "indexing/language_reference"
-require_relative "indexing/query_runner"
 require_relative "indexing/report"
-require_relative "indexing/topic_reference"
 
 module Ld4lBrowserData
   module Indexing
