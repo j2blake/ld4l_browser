9d37afb5098782bc7b04c568a258bf0de01f930e
Jim Blake <jeb228@cornell.edu>
2016-02-23 17:23:13 -0500

    Specify the graph for the queries. Should run faster?

--- lib/ld4l_browser_data/generate_lod/uri_processor.rb
+++ lib/ld4l_browser_data/generate_lod/uri_processor.rb
@@ -14,49 +14,53 @@ module Ld4lBrowserData
       include Utilities::TripleStoreUser
 
       QUERY_OUTGOING = <<-END
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-    CONSTRUCT {
-      ?uri ?p ?o .
-      ?o a ?type .
-      ?o rdfs:label ?label . 
-      ?o skos:prefLabel ?prefLabel . 
-    }
-    WHERE { 
-      ?uri ?p ?o . 
-      OPTIONAL {
-        ?o a ?type .
-      } 
-      OPTIONAL {
-        ?o rdfs:label ?label . 
-      } 
-      OPTIONAL {
-        ?o skos:prefLabel ?prefLabel . 
-      } 
-    }
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+        CONSTRUCT {
+          ?uri ?p ?o .
+          ?o a ?type .
+          ?o rdfs:label ?label . 
+          ?o skos:prefLabel ?prefLabel . 
+        }
+        WHERE { 
+          GRAPH ?g {
+            ?uri ?p ?o . 
+            OPTIONAL {
+              ?o a ?type .
+            } 
+            OPTIONAL {
+              ?o rdfs:label ?label . 
+            } 
+            OPTIONAL {
+              ?o skos:prefLabel ?prefLabel . 
+            }
+          } 
+        }
       END
 
       QUERY_INCOMING = <<-END
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-    CONSTRUCT {
-      ?s ?p ?uri .
-      ?s a ?type . 
-      ?s rdfs:label ?label . 
-      ?s skos:prefLabel ?prefLabel . 
-    }
-    WHERE { 
-      ?s ?p ?uri .
-      OPTIONAL {
-        ?s a ?type . 
-      } 
-      OPTIONAL {
-        ?s rdfs:label ?label . 
-      } 
-      OPTIONAL {
-        ?s skos:prefLabel ?prefLabel . 
-      } 
-    }
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+        CONSTRUCT {
+          ?s ?p ?uri .
+          ?s a ?type . 
+          ?s rdfs:label ?label . 
+          ?s skos:prefLabel ?prefLabel . 
+        }
+        WHERE { 
+          GRAPH ?g {
+            ?s ?p ?uri .
+            OPTIONAL {
+              ?s a ?type . 
+            } 
+            OPTIONAL {
+              ?s rdfs:label ?label . 
+            } 
+            OPTIONAL {
+              ?s skos:prefLabel ?prefLabel . 
+            }
+          } 
+        }
       END
       #
       def initialize(ts, files, report, uri)
@@ -69,8 +73,8 @@ module Ld4lBrowserData
 
       def build_the_graph
         @graph = RDF::Graph.new
-        @graph << QueryRunner.new(QUERY_OUTGOING).bind_uri('uri', @uri).construct(@ts)
-        @graph << QueryRunner.new(QUERY_INCOMING).bind_uri('uri', @uri).construct(@ts)
+        @graph << QueryRunner.new(QUERY_OUTGOING).bind_graph('g', @uri).bind_uri('uri', @uri).construct(@ts)
+        @graph << QueryRunner.new(QUERY_INCOMING).bind_graph('g', @uri).bind_uri('uri', @uri).construct(@ts)
       end
 
       def write_it_out
